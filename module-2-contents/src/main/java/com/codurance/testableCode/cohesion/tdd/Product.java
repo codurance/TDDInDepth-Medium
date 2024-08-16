726594822737ab1a6271fb55ab9b8f772c2033e3
Benjamin Rae <benjaminrae93@gmail.com>
2024-08-16 14:30:37 +0200

    test: shopping cart should calculate total price

---
+++ module-2-contents/src/main/java/com/codurance/testableCode/cohesion/tdd/Product.java
@@ -0,0 +1,7 @@
+package com.codurance.testableCode.cohesion.tdd;
+
+public class Product {
+    public Product(String name, double price) {
+        throw new UnsupportedOperationException("Method not implemented");
+    }
+}
