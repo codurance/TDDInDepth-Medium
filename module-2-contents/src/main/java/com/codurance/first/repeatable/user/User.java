9253b07f53563e6395cc32ae09a173a9ea586b41
Koldo Picaza <koldo.picaza@codurance.com>
2024-08-19 12:13:37 +0200

    feat(repeatable): user repository

---
+++ module-2-contents/src/main/java/com/codurance/first/repeatable/user/User.java
@@ -0,0 +1,14 @@
+package com.codurance.first.repeatable.user;
+
+public class User {
+    private final String username;
+
+    public User(String username) {
+
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
