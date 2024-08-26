9dc553469b0c5809392632c84efbbc768ac0eeee
Koldo Picaza <koldo.picaza@codurance.com>
2024-08-26 11:37:37 +0200

    feat(gilded_rose): step 1 -> characterize the behavior of the "Aged brie"

--- module-2-contents/src/test/java/com/codurance/legacy/gildedrose/characterization/GildedRoseApprovalTest.java
+++ module-2-contents/src/test/java/com/codurance/legacy/gildedrose/characterization/GildedRoseApprovalTest.java
@@ -3,6 +3,7 @@ package com.codurance.legacy.gildedrose.characterization;
 import org.approvaltests.Approvals;
 import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.UseReporter;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -20,6 +21,18 @@ public class GildedRoseApprovalTest {
         app.updateQuality();
 
         Approvals.verifyAll("Items", items);
+    }
+
+    @Test
+    public void shouldIncreaseQualityForAgedBrie() {
+        Item agedBrie = new Item("Aged Brie", 5, 10);
+        Item[] items = new Item[]{agedBrie};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        Assertions.assertEquals(11, agedBrie.quality);
+        Assertions.assertEquals(4, agedBrie.sellIn);
     }
 
     @Test
