e9e17bacdf5c46a2622d79a874baa03ed660f62f
Benjamin Rae <benjaminrae93@gmail.com>
2024-08-26 13:13:26 +0200

    chore: set up testing library

--- module-2-frontend-contents/vite.config.ts
+++ module-2-frontend-contents/vite.config.ts
@@ -1,9 +1,15 @@
 /// <reference types="vitest" />
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {defaultExclude, defaultInclude} from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    test: {}
+    test: {
+        environment: 'jsdom',
+        include: [...defaultInclude],
+        exclude: [...defaultExclude],
+        setupFiles: ["setupTests.ts"],
+    }
 })
