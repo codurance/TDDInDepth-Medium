9c95ad7e587895a0f920c9f5dd129bf3d6c76ced
Koldo Picaza <koldo.picaza@codurance.com>
2024-08-28 09:35:56 +0200

    feat(branch_by_abstraction): step 1 -> create notification client abstraction

--- module-2-contents/src/main/java/com/codurance/legacy/branchbyabstraction/ThirdPartyNotificationClient.java
+++ module-2-contents/src/main/java/com/codurance/legacy/branchbyabstraction/ThirdPartyNotificationClient.java
@@ -2,7 +2,7 @@ package com.codurance.legacy.branchbyabstraction;
 
 import jdk.jshell.spi.ExecutionControl.NotImplementedException;
 
-public final class ThirdPartyNotificationClient {
+public final class ThirdPartyNotificationClient implements NotificationClient {
     public void send(String message) throws NotImplementedException {
         throw new NotImplementedException("Calling third party Service");
     }
