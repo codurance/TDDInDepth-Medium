45fe668a5e809d1162d104c135b76fd66cf5880a
Koldo Picaza <koldo.picaza@codurance.com>
2024-09-05 17:18:13 +0200

    test(tictactoe): create initial referee test

--- tictactoe/src/main/java/Referee.java
+++ tictactoe/src/main/java/Referee.java
@@ -1,7 +1,20 @@
+import java.util.HashSet;
 import java.util.List;
 
 public class Referee {
+
     public boolean hasWon(List<Position> positions) {
-        return positions.size() == 3;
+        var rules = List.of(
+            List.of(
+                Position.TOP_LEFT,
+                Position.TOP,
+                Position.TOP_RIGHT
+            )
+        );
+
+        return rules.stream()
+            .anyMatch(
+                rule -> new HashSet<>(positions).containsAll(rule)
+            );
     }
 }
