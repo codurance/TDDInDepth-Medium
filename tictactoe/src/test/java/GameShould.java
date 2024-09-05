917fb71602387d69f0ea60cfd546e608ea45334d
Koldo Picaza <koldo.picaza@codurance.com>
2024-09-05 12:44:11 +0200

    refactor(tictactoe): extract initial board concept from game

--- tictactoe/src/test/java/GameShould.java
+++ tictactoe/src/test/java/GameShould.java
@@ -1,21 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameShould {
+
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game(new Board());
+    }
+
     @Test
     public void wait_for_player_X_to_play() {
-
-        Game game = new Game();
-
         assertEquals(Player.PLAYER_X, game.toPlay());
     }
 
     @Test
     public void wait_for_player_O_to_play_after_player_X() {
-
-        Game game = new Game();
-
         game.play(Tile.TOP_LEFT);
 
         assertEquals(Player.PLAYER_O, game.toPlay());
@@ -23,9 +26,6 @@ public class GameShould {
 
     @Test
     public void wait_for_player_X_to_play_after_player_O() {
-
-        Game game = new Game();
-
         game.play(Tile.TOP_LEFT);
         game.play(Tile.TOP);
 
@@ -34,8 +34,6 @@ public class GameShould {
 
     @Test
     public void not_allow_players_to_play_on_already_taken_positions() {
-        Game game = new Game();
-
         game.play(Tile.TOP_LEFT);
         game.play(Tile.TOP_LEFT);
 
