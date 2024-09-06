f8697fa6246478923c31b2845215d191dabc33d9
Koldo Picaza <koldo.picaza@codurance.com>
2024-09-06 12:06:36 +0200

    feature(tictactoe): create referee

--- tictactoe/src/main/java/Referee.java
+++ tictactoe/src/main/java/Referee.java
@@ -3,14 +3,51 @@ import java.util.List;
 
 public class Referee {
 
+    private List<List<Position>> rules = List.of(
+        List.of(
+            Position.TOP_LEFT,
+            Position.TOP,
+            Position.TOP_RIGHT
+        ),
+        List.of(
+            Position.CENTER_LEFT,
+            Position.CENTER,
+            Position.CENTER_RIGHT
+        ),
+        List.of(
+            Position.BOTTOM_LEFT,
+            Position.BOTTOM,
+            Position.BOTTOM_RIGHT
+        ),
+
+        List.of(
+            Position.TOP_LEFT,
+            Position.CENTER_LEFT,
+            Position.BOTTOM_LEFT
+        ),
+        List.of(
+            Position.TOP,
+            Position.CENTER,
+            Position.BOTTOM
+        ),
+        List.of(
+            Position.TOP_RIGHT,
+            Position.CENTER_RIGHT,
+            Position.BOTTOM_RIGHT
+        ),
+        List.of(
+            Position.TOP_LEFT,
+            Position.CENTER,
+            Position.BOTTOM_RIGHT
+        ),
+        List.of(
+            Position.TOP_RIGHT,
+            Position.CENTER,
+            Position.BOTTOM_LEFT
+        )
+    );;
+
     public boolean hasWon(List<Position> positions) {
-        var rules = List.of(
-            List.of(
-                Position.TOP_LEFT,
-                Position.TOP,
-                Position.TOP_RIGHT
-            )
-        );
 
         return rules.stream()
             .anyMatch(
