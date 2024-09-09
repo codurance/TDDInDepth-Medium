e7549dae7d1b6407307b1bcbc5e4e5deb1b9c8a3
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-09 12:29:13 +0200

    refactor: create BackstagePassesUpdater

---
+++ gilded-rose/src/main/java/com/gildedrose/BackstagePassesUpdater.java
@@ -0,0 +1,8 @@
+package com.gildedrose;
+
+public class BackstagePassesUpdater extends ItemUpdater {
+    @Override
+    public void update(Item item) {
+        this.updateBackstagePasses(item);
+    }
+}
