a5757af18d430e5b9f8fdd3381ba49a849c9d04d
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-09 12:38:22 +0200

    refactor: create ItemUpdater factory method and make abstract

--- gilded-rose/src/main/java/com/gildedrose/GildedRose.java
+++ gilded-rose/src/main/java/com/gildedrose/GildedRose.java
@@ -10,7 +10,7 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            ItemUpdater itemUpdater = new ItemUpdater();
+            ItemUpdater itemUpdater = ItemUpdater.forItem(item);
             itemUpdater.update(item);
         }
     }
