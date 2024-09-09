843e61091d666527c74aa962749cdd189a05af61
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-09 13:22:11 +0200

    refactor: extract class DenominationCollection

--- atm-machine/src/main/java/AtmMachine.java
+++ atm-machine/src/main/java/AtmMachine.java
@@ -2,18 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AtmMachine {
-    private List<Money> availableDenominations = List.of(Money.coin(2), Money.coin(1));
 
     public List<Money> withdraw(int quantity) {
-        ArrayList<Money> withdrawal = new ArrayList<>();
+        DenominationCollection denominationCollection = new DenominationCollection();
 
-        for (Money denomination : availableDenominations) {
-            if (quantity >= denomination.value()) {
-                withdrawal.add(denomination);
-                quantity -= denomination.value();
+        return denominationCollection.selectDenominationsFor(quantity);
+    }
+
+    private class DenominationCollection {
+        private final List<Money> availableDenominations = List.of(Money.coin(2), Money.coin(1));
+
+        private ArrayList<Money> selectDenominationsFor(int quantity) {
+            ArrayList<Money> withdrawal = new ArrayList<>();
+
+            for (Money denomination : availableDenominations) {
+                if (quantity >= denomination.value()) {
+                    withdrawal.add(denomination);
+                    quantity -= denomination.value();
+                }
             }
+            return withdrawal;
         }
-
-        return withdrawal;
     }
 }
