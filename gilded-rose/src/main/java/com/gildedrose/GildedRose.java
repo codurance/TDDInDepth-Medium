33982971dbb1e9bc34f40e528ae8feebf17e84f1
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-09 11:36:35 +0200

    refactor: isolate elixir of the mongoose (normal item)

--- gilded-rose/src/main/java/com/gildedrose/GildedRose.java
+++ gilded-rose/src/main/java/com/gildedrose/GildedRose.java
@@ -47,6 +47,55 @@ class GildedRose {
                 }
             } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
 
+            } else if (item.name.equals("Elixir of the Mongoose")) {
+                if (!item.name.equals("Aged Brie")
+                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+
+                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (item.sellIn < 11) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.sellIn = item.sellIn - 1;
+                }
+
+                if (item.sellIn < 0) {
+                    if (!item.name.equals("Aged Brie")) {
+                        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (item.quality > 0) {
+                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                    item.quality = item.quality - 1;
+                                }
+                            }
+                        } else {
+                            item.quality = item.quality - item.quality;
+                        }
+                    } else {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
             }
             else {
                 if (!item.name.equals("Aged Brie")
