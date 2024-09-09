00cb589fb54380d41413bfce830ddc3495514a9b
Koldo Picaza <koldo.picaza@codurance.com>
2024-09-09 15:42:13 +0200

    feat(simple_mars_rover): rotate to left all directions

--- SimpleMarsRover/src/main/java/MarsRover.java
+++ SimpleMarsRover/src/main/java/MarsRover.java
@@ -1,5 +1,24 @@
 public class MarsRover {
+    private Direction direction;
+
+    public MarsRover(Direction direction) {
+        this.direction = direction;
+    }
+
     public Direction direction() {
-        return Direction.WEST;
+        return direction;
+    }
+
+    public void execute(String commands) {
+        for (char command : commands.toCharArray()) {
+            if (command == 'L') {
+                switch (direction) {
+                    case NORTH -> direction = Direction.WEST;
+                    case WEST -> direction = Direction.SOUTH;
+                    case SOUTH -> direction = Direction.EAST;
+                    case EAST -> direction = Direction.NORTH;
+                }
+            }
+        }
     }
 }
