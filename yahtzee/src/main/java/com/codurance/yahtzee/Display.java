d058933240660cdef22a20c0bfd38fbcf6edf45b
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-10 12:06:56 +0200

    Revert "feat: yahtzee should display dice rolled by player"
    This reverts commit 9a3ef1f838c16f6aa2a5f15909214396a21dcb1f.

--- yahtzee/src/main/java/com/codurance/yahtzee/Display.java
+++
@@ -1,7 +0,0 @@
-package com.codurance.yahtzee;
-
-public class Display {
-    public void displayRoll(int[] diceRolls) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-}
