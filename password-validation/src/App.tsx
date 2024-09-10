6ffa425ee3fbe346a92ecd9b72199bd4ecd6f677
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-10 14:16:06 +0200

    feat: user should be able to select between short and standard validations

--- password-validation/src/App.tsx
+++ password-validation/src/App.tsx
@@ -1,6 +1,27 @@
-const App = () => (
-    <>
-    </>
-);
+import {PasswordValidationForm} from "./components/PasswordValidationForm.tsx";
+import React, {useState} from "react";
+import {PasswordValidation} from "./PasswordValidator/PasswordValidation.ts";
+import {useValidator} from "./hooks/useValidator.ts";
+
+const App = () => {
+    const [validation, setValidation] = useState<PasswordValidation>()
+    const {validator} = useValidator(validation);
+
+    const handleSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setValidation(event.target.value as PasswordValidation);
+    }
+    return (
+        <>
+            <label htmlFor="strategy">
+                What validation strategy do you want to use?
+            </label>
+            <select id="strategy" defaultValue={PasswordValidation.STANDARD_VALIDATION} onChange={handleSelection}>
+                <option>{PasswordValidation.STANDARD_VALIDATION}</option>
+                <option>{PasswordValidation.SHORT_VALIDATION}</option>
+            </select>
+            <PasswordValidationForm onValidate={validator}/>
+        </>
+    );
+}
 
 export default App
