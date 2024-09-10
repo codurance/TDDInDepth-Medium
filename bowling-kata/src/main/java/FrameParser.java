e18f6e60f09d7f9ea14e0a3e86a7473d733fe53c
Koldo Picaza <koldo.picaza@codurance.com>
2024-09-10 17:11:33 +0200

    refactor(bowling): frame parser readability

--- bowling-kata/src/main/java/FrameParser.java
+++ bowling-kata/src/main/java/FrameParser.java
@@ -2,36 +2,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FrameParser {
+
+    private static final int TOTAL_FRAMES = 10;
+    private static final int MAX_PINS_PER_ROLL = 10;
+    private static final int LAST_FRAME_INDEX = 9;
+    private static final int MAX_ROLLS = 21;
+    private static final int PERFECT_GAME_ROLLS = 12;
+
     public Frames getFramesOf(List<Integer> rolls) {
         List<Frame> frames = new ArrayList<>();
         int index = 0;
-        for (int i = 0; i < 10; i++) {
-            int firstRoll = rolls.get(index);
-            if (10 == firstRoll && i != 9 ) {
-                Frame frame = new Frame(firstRoll, 0);
-                frames.add(frame);
 
-                index += 1;
+        for (int i = 0; i < TOTAL_FRAMES; i++) {
+            int firstRoll = rolls.get(index);
+
+            if (isStrike(rolls, index, i)) {
+                frames.add(new Frame(firstRoll, 0));
+                index++;
                 continue;
             }
 
-            if(i == 9 && hasExtraRoll(rolls)) {
-                Frame frame = new Frame(firstRoll, rolls.get(index + 1), rolls.get(index + 2));
-                frames.add(frame);
-
+            if (isTenthFrameWithExtraRoll(rolls, i)) {
+                frames.add(new Frame(firstRoll, rolls.get(index + 1), rolls.get(index + 2)));
                 index += 3;
-            } else {
-                Frame frame = new Frame(firstRoll, rolls.get(index + 1));
-                frames.add(frame);
-
-                index += 2;
+                continue;
             }
+
+            frames.add(new Frame(firstRoll, rolls.get(index + 1)));
+            index += 2;
         }
 
         return new Frames(frames);
     }
 
-    private static boolean hasExtraRoll(List<Integer> rolls) {
-        return rolls.size() == 12 || rolls.size() == 21;
+    private boolean isStrike(List<Integer> rolls, int index, int frameIndex) {
+        return rolls.get(index) == MAX_PINS_PER_ROLL && frameIndex != LAST_FRAME_INDEX;
+    }
+
+    private boolean isTenthFrameWithExtraRoll(List<Integer> rolls, int frameIndex) {
+        return frameIndex == LAST_FRAME_INDEX && hasExtraRoll(rolls);
+    }
+
+    private boolean hasExtraRoll(List<Integer> rolls) {
+        return rolls.size() == PERFECT_GAME_ROLLS || rolls.size() == MAX_ROLLS;
     }
 }
