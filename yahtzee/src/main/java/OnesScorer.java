6aac2a035443370056dfeac254fa1277dc18e08c
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-10 11:36:41 +0200

    refactor: move score down to scorer subclasses

--- yahtzee/src/main/java/OnesScorer.java
+++ yahtzee/src/main/java/OnesScorer.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class OnesScorer extends Scorer {
     public OnesScorer() {
         super(ScoringCategory.ONES);
+    }
+
+    @Override
+    public int score(int[] diceRolls) {
+        return Arrays.stream(diceRolls).filter(dice -> dice == 1).sum();
     }
 }
