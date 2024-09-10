0eced25515fc7d7b4bb25caa6d3acd7723fcb2ee
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-10 13:00:17 +0200

    refactor: take password value from password input

--- password-validation/src/components/PasswordValidationForm.tsx
+++ password-validation/src/components/PasswordValidationForm.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 type PasswordValidationFormProps = {
     onValidate: (password: string) => boolean;
 }
 
 export const PasswordValidationForm: React.FC<PasswordValidationFormProps> = ({onValidate}) => {
+    const [password, setPassword] = useState("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    }
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-
-        onValidate("Password123_");
+        onValidate(password);
     };
 
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="password">What's the password you want to validate?</label>
-            <input type="password" id="password" name="password"/>
+            <input type="password" id="password" name="password" onChange={handleChange}/>
             <button type="submit">
                 Validate
             </button>
