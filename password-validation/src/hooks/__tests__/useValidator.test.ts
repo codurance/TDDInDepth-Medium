863a7c5a04fa0d1e386deb2cb8bb2b874cab4b98
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-10 13:12:11 +0200

    feat: should return false for a password with 8 characters

--- password-validation/src/hooks/__tests__/useValidator.test.ts
+++ password-validation/src/hooks/__tests__/useValidator.test.ts
@@ -7,4 +7,13 @@ describe("useValidator", () => {
 
         expect(validator("Password123_")).toBe(true);
     })
+
+
+    it.each([
+        ["Pass123_"],
+    ])("should return false for a password with 8 or less characters", (password: string) => {
+        const {validator} = useValidator();
+
+        expect(validator(password)).toBe(false);
+    })
 })
--- password-validation/src/hooks/useValidator.ts
+++ password-validation/src/hooks/useValidator.ts
@@ -1,5 +1,9 @@
 export const useValidator = () => {
     const validator = (password: string): boolean => {
+        if (password.length === 8) {
+            return false;
+        }
+
         return true;
     }
 
