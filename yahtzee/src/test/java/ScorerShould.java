acf73457b2d188fa569d2772132446ef8827d65e
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-10 11:14:06 +0200

    refactor: introduce parameterized test

--- yahtzee/src/test/java/ScorerShould.java
+++ yahtzee/src/test/java/ScorerShould.java
@@ -1,13 +1,26 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScorerShould {
-    @Test
-    public void score_with_ones_scoring_category() {
+    public static Stream<Arguments> onesProvider() {
+        return Stream.of(
+            Arguments.of(
+                1,
+                new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @ParameterizedTest
+    @MethodSource("onesProvider")
+    public void score_with_ones_scoring_category(int expectedScore, int[] dice) {
         Scorer scorer = new Scorer();
 
-        int expectedScore = 1;
-        assertEquals(expectedScore, scorer.score(ScoringCategory.ONES, new int[]{1, 2, 3, 4, 5}));
+        int score = scorer.score(ScoringCategory.ONES, dice);
+
+        assertEquals(expectedScore, score);
     }
 }
